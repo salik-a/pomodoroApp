bce6949bb70855b91f52c910149740fb558a2db0
salik-a <salika@mef.edu.tr>
2021-10-16 21:43:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  
+
+  return (
+    <SafeAreaView >
+      <Text>Pomodoro</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
